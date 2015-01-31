3c775f3cfb068d446e5dd09354ad4fbfe7224ea9
Yaroslav <nychka93@gmail.com>
2015-01-31 12:00:52 +0200

    add Guardfile, CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1 / Unreleased
+### API
+- Authorization using social networks: facebook, vkontaker, gplus
---
+++ spec/spec_helper.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+end
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+Spork.prefork do
+
+  RSpec.configure do |config|
+    # rspec-expectations config goes here. You can use an alternate
+    # assertion/expectation library such as wrong or the stdlib/minitest
+    # assertions if you prefer.
+    config.expect_with :rspec do |expectations|
+      # This option will default to `true` in RSpec 4. It makes the `description`
+      # and `failure_message` of custom matchers include text for helper methods
+      # defined using `chain`, e.g.:
+      # be_bigger_than(2).and_smaller_than(4).description
+      #   # => "be bigger than 2 and smaller than 4"
+      # ...rather than:
+      #   # => "be bigger than 2"
+      expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    end
+
+    # rspec-mocks config goes here. You can use an alternate test double
+    # library (such as bogus or mocha) by changing the `mock_with` option here.
+    config.mock_with :rspec do |mocks|
+      # Prevents you from mocking or stubbing a method that does not exist on
+      # a real object. This is generally recommended, and will default to
+      # `true` in RSpec 4.
+      mocks.verify_partial_doubles = true
+    end
+    config.filter_run :focus
+    config.run_all_when_everything_filtered = false
+    config.order = :random
+    # Print the 10 slowest examples and example groups at the
+    # end of the spec run, to help surface which specs are running
+    # particularly slow.
+    #config.profile_examples = 5
+
+    # Run specs in random order to surface order dependencies. If you find an
+    # order dependency and want to debug it, you can fix the order by providing
+    # the seed, which is printed after each run.
+    #     --seed 1234
+    #config.order = :random
+
+    # Seed global randomization in this process using the `--seed` CLI option.
+    # Setting this allows you to use `--seed` to deterministically reproduce
+    # test failures related to randomization by passing the same `--seed` value
+    # as the one that triggered the failure.
+    # Kernel.srand config.seed
+  end
+end
